fixing formula

rio_polyencode/scripts/cli.py
@@ -7,13 +7,16 @@ import numpy as np
 
 from rio_polyencode import __version__ as polyencode_version
 
-def read_all(inputs):
+def read_all(inputs, reflect):
     with rio.open(inputs[0]) as src:
-        out = np.zeros((len(inputs), src.height, src.width), dtype=src.meta['dtype'])
+        out = np.zeros((len(inputs) + reflect, src.height, src.width), dtype=src.meta['dtype'])
 
     for i, p in enumerate(inputs):
         with rio.open(p) as src:
             src.read(1, out=out[i])
+
+    for r in range(reflect):
+        out[r + len(inputs)] = out[r]
 
     return out
 
@@ -43,9 +46,10 @@ def poly_multid(data, pdegree=2):
     'output',
     type=click.Path(resolve_path=True))
 @click.option('--poly-order', '-d', type=int, default=2)
+@click.option('--reflect', '-r', type=int, default=0)
 @click.version_option(version=polyencode_version, message='%(version)s')
 @click.pass_context
-def polyencode(ctx, inputfiles, output, poly_order):
+def polyencode(ctx, inputfiles, output, poly_order, reflect):
     """
     """
     with rio.open(inputfiles[0]) as src:
@@ -55,7 +59,7 @@ def polyencode(ctx, inputfiles, output, poly_order):
 
     verbosity = (ctx.obj and ctx.obj.get('verbosity')) or 1
 
-    data = read_all(inputfiles)
+    data = read_all(inputfiles, reflect)
     
     out = poly_multid(data, poly_order).astype(np.float32)
 
@@ -73,22 +77,28 @@ def polyencode(ctx, inputfiles, output, poly_order):
     'output',
     type=click.Path(resolve_path=True))
 @click.argument('x', type=int)
+@click.option('--multiplier', '-m', type=float, default=1)
 @click.version_option(version=polyencode_version, message='%(version)s')
 @click.pass_context
-def polydecode(ctx, inputfile, output, x):
+def polydecode(ctx, inputfile, output, x, multiplier):
     """
     """
     with rio.open(inputfile) as src:
         metaprof = src.profile.copy()
         data = src.read()
 
+    x *= multiplier
+
     depth, rows, cols = data.shape
 
-    out = np.sum(np.dstack([p * x ** abs(depth - d) for p, d in zip(data, range(depth))]), axis=2)
+    depth -= 1
+
+    out = (np.sum(np.dstack([p * x ** abs(depth - d) for p, d in zip(data, range(depth))]), axis=2) + data[-1]).astype(np.float32)
 
     metaprof.update(dtype=np.float32, count=1)
 
     verbosity = (ctx.obj and ctx.obj.get('verbosity')) or 1
+
 
     with rio.open(output, 'w', **metaprof) as dst:
         dst.write(out, 1)